8ef567ddc7036bffddfdc4478058c20c549b2e13
Carter David Hinsley <carterhinsley@outlook.com>
2023-06-23 16:08:14 -0400

    Working example, tested homoclinic search

--- AvrutinSearch/src/AvrutinSearch.jl
+++ AvrutinSearch/src/AvrutinSearch.jl
@@ -1,16 +1,20 @@
 module AvrutinSearch
 export homoclinic_to_equilibrium
 
+using Interpolations
 using IntervalArithmetic
 using Peaks
 using Roots
 
 
-function homoclinic_to_equilibrium(f, x_star::Float64, T_0::Interval{Float64}, I::Interval{Float64}, r_max::UInt64, n_iter::UInt64=0, tol::Float64=1e-5)::Union{Bool, Nothing}
+function homoclinic_to_equilibrium(xs::Vector{Float64}, fs::Vector{Float64}, x_star::Float64, T_0::Interval{Float64}, I::Interval{Float64}, r_max::UInt64, n_iter::UInt64=0)::Union{Tuple{Float64, UInt64}, Nothing}
     # Return values:
     # true: Connection from x_star to T found.
     # false: Connection from x_star to T does not exist.
     # nothing: Connection from x_star to T not found.
+
+    # Interpolate f from f_pairs.
+    f = linear_interpolation(xs, fs)
 
     # Check if x_star is a fixed point.
     if f(x_star) != x_star
@@ -27,37 +31,53 @@ function homoclinic_to_equilibrium(f, x_star::Float64, T_0::Interval{Float64}, I
         throw(ArgumentError("T_0 must be a subset of I."))
     end
     
+    # TODO: Implement iterates of intervals for linearly interpolated functions.
     # Check if f(I) = I.
-    if f(I) != I
-        throw(ArgumentError("f(I) must equal I."))
-    end
+    #if f(I) != I
+    #    throw(ArgumentError("f(I) must equal I."))
+    #end
 
     # Compute T as `n_iter'th iterate of T_0.
     T = T_0
-    for _ in 1:n_iter
-        T = f(T)
-    end
+    #for _ in 1:n_iter
+    #    T = f(T)
+    #end
 
     # Compute k, the intervals V_1, ..., V_k and functions f_1^{-1}, ..., f_k^{-1}.
-    sample_points = I.lo:tol:I.hi
+    sample_points = vcat([I.lo], filter(x -> I.lo < x < I.hi, xs), [I.hi])
     sample_values = f.(sample_points)
-    partition = sort(vcat(Peaks.argmaxima(sample_values), Peaks.argminima(sample_values)))
-    V = [Interval(sample_points[partition[i]], sample_points[partition[i+1]]) for i in 1:2:length(partition)-1]
+    partition = sort(vcat(1, Peaks.argmaxima(sample_values), Peaks.argminima(sample_values), length(sample_points)))
+    V = [Interval(sample_points[partition[i]], sample_points[partition[i+1]]) for i in 1:length(partition)-1]
     k = length(V)
-    f_inv = [y -> find_zero((x -> f(x) - y), (V[i].lo, V[i].hi)) for i in 1:k]
+    # Get the ranges of f over each V_i.
+    f_V = []
+    for i in 1:k
+        push!(f_V, Interval(sort([f(V[i].lo), f(V[i].hi)])...))
+    end
+    f_inv = []
+    for i in 1:k
+        # Produce a linearly interpolated local inverse of f.
+        # We have to supply the domain points in increasing order for Interpolations.jl.
+        if sample_values[partition[i]] < sample_values[partition[i+1]] # f is increasing on V_i.
+            f_inv_i = linear_interpolation(sample_values[partition[i]:partition[i+1]], sample_points[partition[i]:partition[i+1]])
+        else # f is decreasing on V_i.
+            f_inv_i = linear_interpolation(sample_values[partition[i+1]:-1:partition[i]], sample_points[partition[i+1]:-1:partition[i]])
+        end
+        push!(f_inv, f_inv_i)
+    end
 
     # Proceed with the algorithm from the paper.
-    S_p = [(x_star, 0)]
-    i_max = 0
+    S_p = [(x_star, UInt64(0))]
+    i_max = UInt64(0)
     while length(S_p) > 0 # While S_p is not empty.
         p, i = popfirst!(S_p)
         if i <= r_max
             i_max = max(i_max, i)
             for j in 1:k
-                if p ∈ V[j]
+                if p ∈ f_V[j]
                     x = f_inv[j](p)
                     if x ∈ T && x != x_star
-                        return true
+                        return (x, i+1)
                     end
                     if x ∈ I && x != x_star
                         push!(S_p, (x, i+1))
@@ -68,9 +88,9 @@ function homoclinic_to_equilibrium(f, x_star::Float64, T_0::Interval{Float64}, I
     end
 
     if i_max == r_max
-        return false
-    else
         return nothing
+    else
+        return (NaN, UInt64(0))
     end
 end
 
---
+++ AvrutinSearch/src/ParabolaMapExample.jl
@@ -0,0 +1,46 @@
+using Pkg
+Pkg.activate("AvrutinSearch")
+
+using Interpolations
+using IntervalArithmetic
+using Roots
+
+using AvrutinSearch
+
+
+xs = collect(0.0:1e-7:1.0)
+f(x) = 4 * (x - x^2)
+fs = f.(xs)
+f_interpolated = linear_interpolation(xs, fs)
+
+I = 0..1
+x_star = find_zero(x -> f_interpolated(x) - x, (0.5, 1))
+T_0 = x_star ± 1e-1
+r_max = UInt64(10)
+n_iter = UInt64(5)
+
+x, i = AvrutinSearch.homoclinic_to_equilibrium(xs, fs, x_star, T_0, I, r_max, n_iter)
+
+using Plots
+
+indices = round.(Int, LinRange(1, length(xs), 100))
+plt = plot([I.lo, I.hi], [I.lo, I.hi], label="Bisectrix", xlims=(I.lo, I.hi), ylims=(I.lo, I.hi), aspect_ratio=:equal)
+plot!(plt, xs[indices], fs[indices], label="f")
+x_forward_iterates = [x]
+for j in 1:i
+    push!(x_forward_iterates, f_interpolated(x_forward_iterates[end]))
+end
+# Make points for cobweb plot of forward iterates.
+x_cobweb = [x_forward_iterates[1]]
+y_cobweb = [x_forward_iterates[1]]
+for j in 1:i
+    push!(x_cobweb, x_forward_iterates[j])
+    push!(y_cobweb, x_forward_iterates[j])
+    push!(x_cobweb, x_forward_iterates[j])
+    push!(y_cobweb, x_forward_iterates[j+1])
+end
+push!(x_cobweb, x_star)
+push!(y_cobweb, x_star)
+# Plot cobweb plot of forward iterates.
+plot!(plt, x_cobweb, y_cobweb, label="Forward iterates")
+display(plt)
